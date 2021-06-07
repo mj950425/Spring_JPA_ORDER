a3d7271acbda8a54b18d3528b3e2cf22efd710bc
minjoon_lee <minjoon1995@naver.com>
2021-06-07 22:26:23 +0900

    [feature] 회원가입,중복체크 서비스,MemberRepository 구현

---
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,52 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+class MemberServiceTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Autowired
+    MemberService memberService;
+
+
+    @Test
+    public void 회원가입() throws Exception{
+        //given
+        Member member = new Member();
+        member.setName("kim");
+        //when
+        Long saveId = memberService.join(member);
+//        System.out.println(member);
+//        System.out.println(memberRepository.findOne(saveId));
+
+        assertEquals(member, memberRepository.findOne(saveId));
+    }
+
+    @Test
+    public void 중복_회원_예외() throws Exception{
+
+        Member member = new Member();
+        Member member1 = new Member();
+
+        member.setName("Lee");
+        member1.setName("Kim");
+        Long saveId = memberService.join(member);
+        try{Long saveId1 = memberService.join(member1);}
+        catch(IllegalStateException e){return;}
+        fail("예외");
+    }
+}
