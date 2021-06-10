6a762a81a234a31f1e27556550e6ab93df0a0258
minjoon_lee <minjoon1995@naver.com>
2021-06-10 22:33:54 +0900

    [feature] itemservice개발

---
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -0,0 +1,34 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ItemService {
+
+    private final ItemRepository itemRepository;
+
+    @Transactional
+    public void saveItem(Item item) {
+        itemRepository.save(item);
+    }
+
+    public List<Item> findItems(Item item) {
+        return itemRepository.findAll();
+    }
+
+    public Item findOen(Long itemId) {
+        return itemRepository.findOne(itemId);
+    }
+
+
+
+
+}
