4798aa1fe4cde8218b1e7adf8ae3c26262e2c875
minjoon_lee <minjoon1995@naver.com>
2021-06-10 22:30:19 +0900

    [feature] Item 로직과 item Repo 구현

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,14 +1,16 @@
 package jpabook.jpashop.domain.item;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Data;
+import lombok.Getter;
 
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Data
+@Getter
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "dtype")
 public abstract class Item {
@@ -28,4 +30,19 @@ public abstract class Item {
     private List<Category> categories = new ArrayList<>();
 
 
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+
+
+    }
+
+    public void removeStock(int quantity) {
+        int resultStock = this.stockQuantity - quantity;
+        if (resultStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+
+        }
+        this.stockQuantity= resultStock;
+
+    }
 }
